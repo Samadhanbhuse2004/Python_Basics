7135bdbd415a5d8f9d420a4e7dda9e0cacaa57d5
Samadhan Bhuse <148427410+Samadhanbhuse2004@users.noreply.github.com>
2026-07-27 21:17:10 +0530

    update problem7.py

--- pynative/problem7.py
+++ pynative/problem7.py
@@ -1,5 +1,5 @@
 def remove_char(word, ch):
-    print("Original word:", word)
+    print("Original word:", word) 
     removed = word[ch:]
     print("Word after removing characters from index", ch, ":", removed)
 remove_char("bhuse samadhan", 4) 
